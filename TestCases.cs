8137cd7b450b802e91f4e7f0e540a8b1ac44043d
Anshuman202 <119556991+Anshuman202@users.noreply.github.com>
2023-01-25 19:28:24 +0530

    updated for uc11

--- TestCases.cs
+++ TestCases.cs
@@ -31,11 +31,16 @@ namespace RegexTestCase
         [TestMethod]
         public void UseCase3()
         {
-            string email = "abc.xyz@bl.co.in";
-            bool expected = true;
-            demo program = new demo();
-            bool actual = program.validmail(email);
-            Assert.AreEqual(expected, actual);
+            string[] emails = new string[3] { "abc.xyz@bl.co.in", "a123@bl.co.in", "fhjjm33@bl.co" };
+           
+            for (int i = 0; i < 3; i++)
+            {
+               
+                bool expected = true;
+                demo program = new demo();
+                bool actual = program.validmail(emails[i]);
+                Assert.AreEqual(expected, actual);
+            }
         }
 
         [TestMethod]
@@ -70,7 +75,7 @@ namespace RegexTestCase
         [TestMethod]
         public void UseCase7()
         {
-            string password = "Praveen9";
+            string password = "Prakash9";
             bool expected = true;
             demo program = new demo();
             bool actual = program.validpwd3(password);
@@ -80,7 +85,7 @@ namespace RegexTestCase
         [TestMethod]
         public void UseCase8()
         {
-            string password = "Pravee$9";
+            string password = "Prakashe$9";
             bool expected = true;
             demo program = new demo();
             bool actual = program.validpwd(password);
